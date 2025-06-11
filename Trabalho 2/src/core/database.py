bb226ae9b2ab0a35632bc509dbb07e294088723e
Mario Aragao <marioestudos.nuvem@gmail.com>
2025-06-11 13:39:34 -0300

    final commit

--- Trabalho 2/src/core/database.py
+++ Trabalho 2/src/core/database.py
@@ -3,6 +3,7 @@ from sqlmodel import SQLModel, create_engine, Session
 
 engine = create_engine("sqlite:///src/core/vetclin.db")
 
+DATABASE_URL = "sqlite:///vetclin.db"
 
 def get_session():
     with Session(engine) as session:
